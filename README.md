be99abacff51cbb2f498e899a2f2d42d322f595e
CCharlieLi <ccharlieli@live.com>
2016-08-02 17:02:59 +0800

    update package info

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 [![Build Status](https://travis-ci.org/CCharlieLi/promisified-npm.svg?branch=master)](https://travis-ci.org/CCharlieLi/promisified-npm)
 
-Pure promisified npm node module.
+Pure promisified `npm` node module. Use entire `npm` in a promise way.
+
+- Compare with others:
+  - npm-as-promised: Only `load` and `commands` in `npm`.
+  - promisify-npm: It's a wrap of `npm command line`, it's not original `npm` at all.
+
 
 # Install
 
@@ -26,6 +31,8 @@ npm.loadAsync(npmConfig).then(() => {
   });
 });
 ```
+
+For more usage of promisified npm, please check `test` directory.
 
 # License
 
--- test/npm.test.js
+++ test/npm.test.js
@@ -8,11 +8,39 @@ const npmConfig = {
 };
 
 describe('NPM promisify test', () => {
-  it('should return promisified commands', (done) => {
+  it('should return list packages info', (done) => {
     npm.loadAsync(npmConfig).then(() => {
       return npm.commands.lsAsync('', true).then((data) => {
         Object.keys(data).length.should.be.above(0);
         data.name.should.be.equal('promisified-npm');
+        done();
+      });
+    }, done).catch(done);
+  });
+
+  it('should return outdated package info', (done) => {
+    npm.loadAsync(npmConfig).then(() => {
+      return npm.commands.outdatedAsync('', true).then((data) => {
+        data.length.should.be.above(0);
+        done();
+      });
+    }, done).catch(done);
+  });
+
+  it('should return view package info', (done) => {
+    npm.loadAsync(npmConfig).then(() => {
+      return npm.commands.viewAsync(['jscs'], true).then((data) => {
+        Object.keys(data[Object.keys(data)[0]]).length.should.be.above(0);
+        done();
+      });
+    }, done).catch(done);
+  });
+
+  it('should return install package info', function (done) {
+    this.timeout(180000);
+    npm.loadAsync(npmConfig).then(() => {
+      return npm.commands.installAsync('.', ['jscs@3.0.0']).then((data) => {
+        data.length.should.be.above(0);
         done();
       });
     }, done).catch(done);
